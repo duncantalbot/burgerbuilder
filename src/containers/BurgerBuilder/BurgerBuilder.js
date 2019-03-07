fa70f02621b51783be6c191dcb8acec1f59e79e6
Duncan Talbot <duncan_talbot@hotmail.com>
2019-03-07 11:03:30 +1100

    Modal Close

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -74,6 +74,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true});
     }
 
+    puchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -85,7 +89,7 @@ class BurgerBuilder extends Component {
 
         return (
             <Auxiliary>
-                <Modal show={this.state.purchasing}>
+                <Modal modalClosed={this.puchaseCancelHandler} show={this.state.purchasing}>
                     <OrderSummary ingredients={this.state.ingredients}/></Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
