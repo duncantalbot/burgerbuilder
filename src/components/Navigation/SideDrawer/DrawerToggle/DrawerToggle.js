5579de5376b3f9e6773d251a42111d11a66e3628
Duncan Talbot <duncan_talbot@hotmail.com>
2019-03-07 19:16:14 +1100

    SideDrawer

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -3,11 +3,11 @@ import classes from './DrawerToggle.css';
 
 const drawerToggle = (props) => {
     return (
-    <div className={classes.DrawerToggle} onClick={props.clicked}>
-        <div></div>
-        <div></div>
-        <div></div>
-    </div>
+        <div className={classes.DrawerToggle} onClick={props.clicked}>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
     )
 };
 
