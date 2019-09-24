5f8485bfe427bb1c319591fc5c7578309f22c873
Duncan Talbot <duncan_talbot@hotmail.com>
2019-09-24 20:02:36 +1000

    Updates

---
+++ precache-manifest.da85994b9ab793bfcb7dd5ae6b73248f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerbuilder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "54bf60a7f6cb7bb60cab",
+    "url": "/burgerbuilder/static/js/runtime~main.54bf60a7.js"
+  },
+  {
+    "revision": "43a78898b8c58d8654ef",
+    "url": "/burgerbuilder/static/js/main.43a78898.chunk.js"
+  },
+  {
+    "revision": "c6811253b61d9bda65cc",
+    "url": "/burgerbuilder/static/js/2.c6811253.chunk.js"
+  },
+  {
+    "revision": "43a78898b8c58d8654ef",
+    "url": "/burgerbuilder/static/css/main.ca01facc.chunk.css"
+  },
+  {
+    "revision": "7e60bda51c1cc5442a9a5deacbe57d5a",
+    "url": "/burgerbuilder/index.html"
+  }
+];
